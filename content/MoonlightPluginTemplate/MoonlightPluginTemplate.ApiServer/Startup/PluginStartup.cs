19ad643519390658594114e3925b67cf54341644
ChiaraBm <chiara.bm04@gmail.com>
2024-12-10 22:33:50 +0100

    Updated plugin template to use the new bundler

--- content/MoonlightPluginTemplate/MoonlightPluginTemplate.ApiServer/Startup/PluginStartup.cs
+++ content/MoonlightPluginTemplate/MoonlightPluginTemplate.ApiServer/Startup/PluginStartup.cs
@@ -1,15 +1,18 @@
 using MoonCore.Extensions;
 using Moonlight.ApiServer.Interfaces.Startup;
+using Moonlight.ApiServer.Services;
 
 namespace MoonlightPluginTemplate.ApiServer.Startup;
 
 public class PluginStartup : IAppStartup
 {
     private readonly ILogger<PluginStartup> Logger;
+    private readonly BundleService BundleService;
 
-    public PluginStartup(ILogger<PluginStartup> logger)
+    public PluginStartup(ILogger<PluginStartup> logger, BundleService bundleService)
     {
         Logger = logger;
+        BundleService = bundleService;
     }
 
     public Task BuildApp(IHostApplicationBuilder builder)
@@ -18,6 +21,9 @@ public class PluginStartup : IAppStartup
         
         // Scan the current plugin assembly for di services
         builder.Services.AutoAddServices<PluginStartup>();
+        
+        // Add css to bundle
+        BundleService.BundleCss("css/MoonlightPluginTemplate.min.css");
         
         return Task.CompletedTask;
     }
